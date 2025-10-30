80d0821a542d4b3a6c86c66dd08fb54e069ac05e
Colabear754 <jky2406@gmail.com>
2025-10-30 22:09:20 +0900

    ✨[Feat] 공통 예외 처리

--- src/main/kotlin/com/colabear754/kbo_crawler/dto/global/GlobalResponse.kt
+++ src/main/kotlin/com/colabear754/kbo_crawler/dto/global/GlobalResponse.kt
@@ -6,4 +6,14 @@ data class GlobalResponse<T>(
     val code: HttpStatus,
     val message: String? = null,
     val data: T? = null
-)
+) {
+    companion object {
+        fun <T> success(data: T? = null): GlobalResponse<T> {
+            return GlobalResponse(HttpStatus.OK, "성공", data)
+        }
+
+        fun error(code: HttpStatus, message: String): GlobalResponse<*> {
+            return GlobalResponse(code, message, null)
+        }
+    }
+}
---
+++ src/main/kotlin/com/colabear754/kbo_crawler/handlers/GlobalExceptionHandler.kt
@@ -0,0 +1,32 @@
+package com.colabear754.kbo_crawler.handlers
+
+import com.colabear754.kbo_crawler.dto.global.GlobalResponse
+import com.fasterxml.jackson.databind.exc.InvalidFormatException
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.http.converter.HttpMessageNotReadableException
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+
+@RestControllerAdvice
+class GlobalExceptionHandler {
+    @ExceptionHandler(HttpMessageNotReadableException::class)
+    fun handleHttpMessageNotReadableException(e: HttpMessageNotReadableException): ResponseEntity<GlobalResponse<*>> {
+        val message = when (val cause = e.cause) {
+            is InvalidFormatException -> {
+                val fieldName = cause.path?.lastOrNull()?.fieldName ?: "Unknown Field"
+                if (cause.targetType?.isEnum == true) "'$fieldName'에 유효하지 않은 값이 입력되었습니다."
+                else "'$fieldName'에 유효하지 않은 형식의 값이 입력되었습니다."
+            }
+            else -> "요청 형식이 올바르지 않습니다."
+        }
+        return ResponseEntity.badRequest().body(GlobalResponse.error(HttpStatus.BAD_REQUEST, message))
+    }
+
+    @ExceptionHandler(Exception::class)
+    fun handleGenericException(e: Exception): ResponseEntity<GlobalResponse<*>> {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .body(GlobalResponse.error(HttpStatus.INTERNAL_SERVER_ERROR, "서버 오류가 발생했습니다."))
+    }
+}
