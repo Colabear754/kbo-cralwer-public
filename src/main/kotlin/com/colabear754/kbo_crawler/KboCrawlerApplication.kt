f56648a3c598f793fc63112044f7139a723bad7e
Colabear754 <jky2406@gmail.com>
2025-10-30 10:21:44 +0900

    🎉Initial Commit

---
+++ src/main/kotlin/com/colabear754/kbo_crawler/KboCrawlerApplication.kt
@@ -0,0 +1,11 @@
+package com.colabear754.kbo_crawler
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KboCrawlerApplication
+
+fun main(args: Array<String>) {
+    runApplication<KboCrawlerApplication>(*args)
+}
