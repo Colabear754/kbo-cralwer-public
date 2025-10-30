Colabear754/kbo-scraper

✨[Feat] 경기 정보 도메인

src/main/kotlin/com/colabear754/kbo_scraper/api/domain/CancellationReason.kt
@@ -1,0 +1,26 @@
+package com.colabear754.kbo_scraper.api.domain
+
+enum class CancellationReason(
+    val reasonName: String
+) {
+    GROUND_CONDITION("그라운드 사정"),
+    RAIN("우천"),
+    HEATWAVE("폭염"),
+    FINE_DUST("미세먼지"),
+    STRONG_WIND("강풍"),
+    YELLOW_DUST("황사"),
+    ETC("기타");
+
+    companion object {
+        fun fromString(reason: String) = when {
+            "그라운드" in reason -> GROUND_CONDITION
+            "우천" in reason -> RAIN
+            "폭염" in reason -> HEATWAVE
+            "미세먼지" in reason -> FINE_DUST
+            "강풍" in reason -> STRONG_WIND
+            "황사" in reason -> YELLOW_DUST
+            "-" == reason -> null
+            else -> ETC
+        }
+    }
+}

src/main/kotlin/com/colabear754/kbo_scraper/api/domain/GameStatus.kt
@@ -1,0 +1,10 @@
+package com.colabear754.kbo_scraper.api.domain
+
+enum class GameStatus(
+    val statusName: String
+) {
+    SCHEDULED("예정"),
+    CANCELED("취소"),
+    PLAYING("경기 중"),
+    FINISHED("경기 종료")
+}

src/main/kotlin/com/colabear754/kbo_scraper/api/domain/Team.kt
@@ -1,0 +1,22 @@
+package com.colabear754.kbo_scraper.api.domain
+
+enum class Team(
+    val teamName: String,
+    val teamFullName: String
+) {
+    DOOSAN("두산", "두산 베어스"),
+    SAMSUNG("삼성", "삼성 라이온즈"),
+    LOTTE("롯데", "롯데 자이언츠"),
+    HANWHA("한화", "한화 이글스"),
+    LG("LG", "LG 트윈스"),
+    KIA("KIA", "KIA 타이거즈"),
+    HEROES("키움", "키움 히어로즈"),
+    NC("NC", "NC 다이노스"),
+    KT("KT", "KT 위즈"),
+    SSG("SSG", "SSG 랜더스"),
+    UNKNOWN("알수없음", "알수없음");
+
+    companion object {
+        fun findByTeamName(teamName: String) = entries.find { it.teamName == teamName } ?: UNKNOWN
+    }
+}

Colabear754/kbo-cralwer-public

src/main/kotlin/com/colabear754/kbo_crawler/domain/enums/CancellationReason.kt
@@ -1,0 +1,26 @@
+package com.colabear754.kbo_crawler.domain.enums
+
+enum class CancellationReason(
+    val reasonName: String
+) {
+    GROUND_CONDITION("그라운드 사정"),
+    RAIN("우천"),
+    HEATWAVE("폭염"),
+    FINE_DUST("미세먼지"),
+    STRONG_WIND("강풍"),
+    YELLOW_DUST("황사"),
+    ETC("기타");
+
+    companion object {
+        fun fromString(reason: String) = when {
+            "그라운드" in reason -> GROUND_CONDITION
+            "우천" in reason -> RAIN
+            "폭염" in reason -> HEATWAVE
+            "미세먼지" in reason -> FINE_DUST
+            "강풍" in reason -> STRONG_WIND
+            "황사" in reason -> YELLOW_DUST
+            "-" == reason -> null
+            else -> ETC
+        }
+    }
+}

src/main/kotlin/com/colabear754/kbo_crawler/domain/enums/GameStatus.kt
@@ -1,0 +1,10 @@
+package com.colabear754.kbo_crawler.domain.enums
+
+enum class GameStatus(
+    val statusName: String
+) {
+    SCHEDULED("예정"),
+    CANCELED("취소"),
+    PLAYING("경기 중"),
+    FINISHED("경기 종료")
+}

src/main/kotlin/com/colabear754/kbo_crawler/domain/enums/Team.kt
@@ -1,0 +1,22 @@
+package com.colabear754.kbo_crawler.domain.enums
+
+enum class Team(
+    val teamName: String,
+    val teamFullName: String
+) {
+    DOOSAN("두산", "두산 베어스"),
+    SAMSUNG("삼성", "삼성 라이온즈"),
+    LOTTE("롯데", "롯데 자이언츠"),
+    HANWHA("한화", "한화 이글스"),
+    LG("LG", "LG 트윈스"),
+    KIA("KIA", "KIA 타이거즈"),
+    HEROES("키움", "키움 히어로즈"),
+    NC("NC", "NC 다이노스"),
+    KT("KT", "KT 위즈"),
+    SSG("SSG", "SSG 랜더스"),
+    UNKNOWN("알수없음", "알수없음");
+
+    companion object {
+        fun findByTeamName(teamName: String) = entries.find { it.teamName == teamName } ?: UNKNOWN
+    }
+}