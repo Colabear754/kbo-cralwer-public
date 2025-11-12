779c98ea73038c0ceadf50f78663c21f00ce065b
Colabear754 <jky2406@gmail.com>
2025-11-12 11:19:53 +0900

    ♻️[Refactor] 공통 응답 포맷 적용방식 수정
    패키지 범위를 제한하여 컨트롤러 클래스만 대상으로 적용

--- src/main/kotlin/com/colabear754/kbo_crawler/api/handlers/GlobalResponseHandler.kt
+++ src/main/kotlin/com/colabear754/kbo_crawler/api/handlers/GlobalResponseHandler.kt
@@ -2,23 +2,29 @@ package com.colabear754.kbo_crawler.api.handlers
 
 import com.colabear754.kbo_crawler.api.dto.global.GlobalResponse
 import org.springframework.core.MethodParameter
-import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.http.converter.HttpMessageConverter
 import org.springframework.http.server.ServerHttpRequest
 import org.springframework.http.server.ServerHttpResponse
-import org.springframework.http.server.ServletServerHttpResponse
 import org.springframework.web.ErrorResponse
 import org.springframework.web.bind.annotation.RestControllerAdvice
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice
 
-@RestControllerAdvice
+@RestControllerAdvice(basePackages = ["com.colabear754.kbo_crawler.api.controllers"])
 class GlobalResponseHandler : ResponseBodyAdvice<Any?> {
+    companion object {
+        private val unSupportedTypes = setOf(
+            GlobalResponse::class.java,
+            ErrorResponse::class.java,
+            Unit::class.java
+        )
+    }
+
     override fun supports(
         returnType: MethodParameter,
         converterType: Class<out HttpMessageConverter<*>?>
     ): Boolean {
-        return true
+        return returnType.parameterType !in unSupportedTypes
     }
 
     override fun beforeBodyWrite(
@@ -29,13 +35,6 @@ class GlobalResponseHandler : ResponseBodyAdvice<Any?> {
         request: ServerHttpRequest,
         response: ServerHttpResponse
     ): Any? {
-        return when (body?.javaClass) {
-            GlobalResponse::class.java, ErrorResponse::class.java -> body
-            else -> GlobalResponse(
-                HttpStatus.valueOf((response as? ServletServerHttpResponse)?.servletResponse?.status ?: 500),
-                "성공",
-                body
-            )
-        }
+        return GlobalResponse.success(body)
     }
 }
