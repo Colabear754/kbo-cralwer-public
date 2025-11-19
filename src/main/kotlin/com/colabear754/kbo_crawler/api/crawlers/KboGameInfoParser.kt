15e024102c68bc3d83ee37a002cd4e93cd12d64c
Colabear754 <jky2406@gmail.com>
2025-11-19 11:31:43 +0900

    ♻️[Refactor] 데이터 파싱 로직 리팩토링

--- src/main/kotlin/com/colabear754/kbo_crawler/api/crawlers/KboGameInfoParser.kt
+++ src/main/kotlin/com/colabear754/kbo_crawler/api/crawlers/KboGameInfoParser.kt
@@ -17,41 +17,37 @@ internal fun parseGameSchedule(locators: List<Locator>, season: Int, seriesType:
 
     var currentDate = LocalDate.MIN  // 날짜 지정용 변수
 
-    for (locator in locators) {
-        val cells = locator.locator("td").all()
-        // 데이터가 없으면 스킵
-        if (cells.size == 1 || cells.first().innerText().contains("데이터가 없습니다.")) {
+    for (row in locators) {
+        // 경기 정보가 없으면 스킵
+        val playCell = row.locator("td.play")
+        if (playCell.count() == 0) {
             continue
         }
-        // 이동일은 스킵
-        if (cells.last().innerText().trim() == "이동일") {
-            continue
-        }
-        // 날짜가 있으면 날짜를 갱신하고 시작지점 = 1, 아니면 시작지점 = 0
-        val startIndex = if (cells.first().getAttribute("class")?.contains("day") == true) {
-            val (month, day) = cells.first().innerText().take(5).trim().split('.').map { it.toInt() }
-            currentDate = LocalDate.of(season, month, day)
-            1
-        } else 0
 
-        val timeText = cells[startIndex].innerText().replace("<b>", "").replace("</b>", "").trim()
-        val time = LocalTime.parse(timeText)
-        // 경기 정보 파싱
-        val playRecord = cells[startIndex + 1].locator("span").allInnerTexts()
-        val awayTeam = Team.findByTeamName(playRecord.first())
-        val homeTeam = Team.findByTeamName(playRecord.last())
+        val dayCell = row.locator("td.day")
+        if (dayCell.count() > 0) {
+            val (month, day) = dayCell.innerText().take(5).trim().split('.').map { it.toInt() }
+            currentDate = LocalDate.of(season, month, day)
+        }
+
+        val time = LocalTime.parse(row.locator("td.time").innerText().trim())
+
+        val (awayTeam, homeTeam) = playCell.locator("> span").allInnerTexts().map { Team.findByTeamName(it) }
+        val scoreSpans = playCell.locator("em span").allInnerTexts()
         var awayScore: Int? = null
         var homeScore: Int? = null
-        if (playRecord.size > 3) {
-            awayScore = playRecord[1].toInt()
-            homeScore = playRecord[3].toInt()
+        if (scoreSpans.size > 1) {
+            awayScore = scoreSpans.first().toIntOrNull()
+            homeScore = scoreSpans.last().toIntOrNull()
         }
 
         val gameKey = "${currentDate.format(yyyyMMdd)}-$awayTeam-$homeTeam"
         val count = gameCountMap[gameKey] ?: 1
         gameCountMap[gameKey] = count + 1
 
-        val cancellationReason = CancellationReason.fromString(cells[startIndex + 7].innerText().trim())
+        val remainCells = row.locator("td:not([class])").all()
+
+        val cancellationReason = CancellationReason.fromString(remainCells.last().innerText().trim())
         val gameStatus = when {
             awayScore != null -> GameStatus.FINISHED
             cancellationReason != null -> GameStatus.CANCELED
@@ -67,8 +63,8 @@ internal fun parseGameSchedule(locators: List<Locator>, season: Int, seriesType:
             awayTeam,
             homeScore,
             awayScore,
-            cells[startIndex + 6].innerText().trim(),
-            cells[startIndex + 4].innerHTML().replace("<br>", ",").trim(),
+            remainCells[3].innerText().trim(),
+            remainCells[1].innerHTML().replace("<br>", ",").trim(),
             gameStatus,
             cancellationReason
         )
